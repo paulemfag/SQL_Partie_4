35d4d1a3da4c4b72fb5c4317d7df1326abd16dae
paulemfag <boiteprofagot@gmail.com>
2020-01-20 15:13:36 +0100

    *

---
+++ source.sql
@@ -0,0 +1,13 @@
+USE webDevelopment;
+INSERT INTO languagess (`language`, `versions`) VALUES ('JavaScript', '5');
+INSERT INTO languagess (`language`, `versions`) VALUES ('PHP', '5.2');
+INSERT INTO languagess (`language`, `versions`) VALUES ('PHP', '5.4');
+INSERT INTO languagess (`language`, `versions`) VALUES ('HTML', '5.1');
+INSERT INTO languagess (`language`, `versions`) VALUES ('JavaScript', '6');
+INSERT INTO languagess (`language`, `versions`) VALUES ('JavaScript', '7');
+INSERT INTO languagess (`language`, `versions`) VALUES ('JavaScript', '8');
+INSERT INTO languagess (`language`, `versions`) VALUES ('PHP', '7');
+INSERT INTO frameworks(`frameworks`, `version`) VALUES ('Symfony', '2.8');
+INSERT INTO frameworks(`frameworks`, `version`) VALUES ('Symfony', '3');
+INSERT INTO frameworks(`frameworks`, `version`) VALUES ('Jquery', '1.6');
+INSERT INTO frameworks(`frameworks`, `version`) VALUES ('Jquery', '2.10');
